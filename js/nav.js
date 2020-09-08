ef706c7cb55e423bdec887ae164d12ade92db9d8
Shaun van Ardenne <s.vanardenne@gmail.com>
2020-09-08 16:18:57 -0700

    added a new color scheme, about page, linked the project card to their respective sites, and fixed the JS for the nav menu

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,9 @@
 /* ============================================= */
 
 const nav = document.querySelector(".nav");
+const html = document.getElementById('html');
+
+console.log(html)
 
 // Removes the navigation panel from the header when the user
 // scrolls down the page
@@ -13,4 +16,12 @@ window.addEventListener('scroll', (e) => {
     } else {
         nav.style.display ="block";
     }
+    if (nav.style.display === "none" && window.innerWidth < 768) {
+        html.style.marginTop = '140px';
+    } else if (nav.style.display === "block" && window.innerWidth < 768) {
+        html.style.marginTop = '320px';
+    }
 });
+
+
+
