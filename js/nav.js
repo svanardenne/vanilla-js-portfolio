a998cb455b00f1a7216c2e45fca605eaa5fedd0d
Shaun van Ardenne <s.vanardenne@gmail.com>
2020-09-09 13:59:03 -0700

    added modal window and linked it to clicks on the projects tab

--- js/nav.js
+++ js/nav.js
@@ -5,23 +5,20 @@
 const nav = document.querySelector(".nav");
 const html = document.getElementById('html');
 
-console.log(html)
-
 // Removes the navigation panel from the header when the user
 // scrolls down the page
+let prevScrollpos = window.pageYOffset;
 window.addEventListener('scroll', (e) => {
-    let offset = window.pageYOffset;
-    if (offset > 0 && window.innerWidth < 768) {
-        nav.style.display ="none";
-    } else {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos && window.innerWidth < 768) {
         nav.style.display ="block";
+    } else {
+        nav.style.display ="none";
     }
     if (nav.style.display === "none" && window.innerWidth < 768) {
         html.style.marginTop = '140px';
     } else if (nav.style.display === "block" && window.innerWidth < 768) {
         html.style.marginTop = '320px';
     }
+    prevScrollpos = currentScrollPos;
 });
-
-
-
