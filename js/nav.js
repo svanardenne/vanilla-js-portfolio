202d5c7480bb6cab789ce49bbbd4e6f32cf0a6db
Shaun van Ardenne <s.vanardenne@gmail.com>
2020-09-09 14:05:48 -0700

    added modal window and linked it to clicks on the projects tab

--- js/nav.js
+++ js/nav.js
@@ -5,20 +5,22 @@
 const nav = document.querySelector(".nav");
 const html = document.getElementById('html');
 
-// Removes the navigation panel from the header when the user
-// scrolls down the page
-let prevScrollpos = window.pageYOffset;
-window.addEventListener('scroll', (e) => {
-    let currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos && window.innerWidth < 768) {
-        nav.style.display ="block";
-    } else {
-        nav.style.display ="none";
-    }
-    if (nav.style.display === "none" && window.innerWidth < 768) {
-        html.style.marginTop = '140px';
-    } else if (nav.style.display === "block" && window.innerWidth < 768) {
-        html.style.marginTop = '320px';
-    }
-    prevScrollpos = currentScrollPos;
-});
+// Toggles the navigation panel when the user
+// scrolls up or down on mobile screens
+if (window.innerWidth < 768) {
+    let prevScrollpos = window.pageYOffset;
+    window.addEventListener('scroll', () => {
+        let currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            nav.style.display ="block";
+        } else {
+            nav.style.display ="none";
+        }
+        if (nav.style.display === "none" && window.innerWidth < 768) {
+            html.style.marginTop = '140px';
+        } else if (nav.style.display === "block" && window.innerWidth < 768) {
+            html.style.marginTop = '320px';
+        }
+        prevScrollpos = currentScrollPos;
+    });
+}
